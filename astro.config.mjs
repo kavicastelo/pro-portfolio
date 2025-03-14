ab893a6aceb92835af40b938b7279dbdd4ccf91e
kavi castelo <kavindu123450@gmail.com>
2025-03-15 03:14:14 +0530

    project structure and hero component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+
+import react from '@astrojs/react';
+
+import tailwindcss from '@tailwindcss/vite';
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), partytown()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
