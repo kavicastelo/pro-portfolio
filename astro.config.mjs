470daff5b4cb91d74381bd30cc0a339c475efa53
kavi castelo <kavindu123450@gmail.com>
2025-03-16 23:03:15 +0530

    contact, header and footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,10 @@ import { defineConfig } from 'astro/config'
 import react from '@astrojs/react';
 
 import tailwindcss from '@tailwindcss/vite';
-import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), partytown()],
+  integrations: [react()],
 
   vite: {
     plugins: [tailwindcss()]
